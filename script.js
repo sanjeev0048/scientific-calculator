9793c09258afb6d94b314afdf1e80e42b70d1d1a
sanjeev0048 <sanjeev04084@gmail.com>
2025-04-19 16:50:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let display = document.getElementById("display");
+
+function appendCharacter(character) {
+    display.value += character;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        alert("Invalid Expression");
+        clearDisplay();
+    }
+}
